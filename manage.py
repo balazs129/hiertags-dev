fcfe4cdb7c2f0439e07ce1998b942b7c6b2040e1
Lovas Bence <lovas.bence@codehouse.hu>
2013-12-19 20:29:20 +0100

    Small bugfixes

--- manage.py
+++ manage.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
-import socket
-
-STAGING_HOSTS = set(['piko1', 'hal'])
 
 if __name__ == "__main__":
-    current_host = socket.gethostname()
-    if current_host in STAGING_HOSTS:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hiertags.settings")
-    else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hiertags.dev_settings")
-        print "dev settings"
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hiertags.settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
