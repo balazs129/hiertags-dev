1b661f2191de99926d5cd03ef876db4374cee325
Balazs <balazs129@gmail.com>
2014-07-04 09:37:05 +0200

    removed unnecessary comments

--- hiertags/settings.py
+++ hiertags/settings.py
@@ -25,39 +25,25 @@ SECRET_KEY = 'xfgfa7a*+n&v8d(nfv6s)@+*^7nqe0&fr18ursn_cm%-t1fk^i'
 # = Global Settings =
 # ===================
 
-# Never deploy a site into production with DEBUG turned on. If your app raises an exception when
-# DEBUG is True, Django will display a detailed traceback,
-# including a lot of metadata about your environment, such as all the currently
-# defined Django settings
+# Never deploy a site into production with DEBUG turned on. I
 DEBUG = False
-# A boolean that turns on/off template debug mode. If this is True, the fancy error page will display
-# a detailed report for any exception raised during template rendering.
-# This report contains the relevant snippet of the template, with the appropriate line highlighted.
+# Detailed report for any exception raised during template rendering.
 TEMPLATE_DEBUG = False
 
 MANAGERS = ADMINS
 TIME_ZONE = 'Europe/Budapest'
 LANGUAGE_CODE = 'en-us'
-# A boolean that specifies if datetimes will be timezone-aware by default or not.
-# If this is set to True, Django will use timezone-aware datetimes internally.
-# Otherwise, Django will use naive datetimes in local time.
+#Use timezones
 USE_TZ = True
 SITE_ID = 1
-# A boolean that specifies whether Djangos translation system should be enabled.
-# This provides an easy way to turn it off, for performance. If this is set to False,
-# Django will make some optimizations so as not to load the translation machinery.
+# Translation system
 USE_I18N = False
 # URL to use when referring to static files located in STATIC_ROOT.
 STATIC_URL = '/static/'
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, '../static')
-# A list of strings representing the host/domain names that this Django site can serve.
-# This is a security measure to prevent an attacker from poisoning caches and password
-# reset emails with links to malicious hosts by submitting requests with a fake HTTP Host header,
-# which is possible even under many seemingly-safe web server configurations.
 ALLOWED_HOSTS = ['*']
-# This setting defines the additional locations the staticfiles app will traverse if the
-# FileSystemFinder finder is enabled
+# Additional locations the staticfiles app will traverse if the FileSystemFinder finder is enabled
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '../media'),
 )
@@ -65,8 +51,7 @@ STATICFILES_DIRS = (
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, '../templates'),
 )
-# List of directories searched for fixture files, in addition to the fixtures
-# directory of each application, in search order.
+# List of directories searched for fixture files
 FIXTURE_DIRS = (
     os.path.join(BASE_DIR, '../conf'),
 )
@@ -112,8 +97,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.media',
     'hiertags.context_processors.act_menu',
 )
-# A tuple of strings designating all applications that are enabled in this Django installation.
-# Each string should be a full Python path to a Python package that contains a Django application
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -132,3 +116,8 @@ INSTALLED_APPS = (
 
 ROOT_URLCONF = 'hiertags.urls'
 WSGI_APPLICATION = 'hiertags.wsgi.application'
+
+try:
+	from settings_dev import *
+except ImportError, exp:
+	pass
